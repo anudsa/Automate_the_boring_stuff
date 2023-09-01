54b96f56f851e1a8b7976b60234a0732a744422a
anudsa <133996511+anudsa@users.noreply.github.com>
2023-08-31 22:49:07 -0600

    Update seleniumWait.py

--- Using python for automation/Selenium/seleniumWait.py
+++ Using python for automation/Selenium/seleniumWait.py
@@ -7,7 +7,7 @@ driver = webdriver.Chrome()
 driver.get('https://www.google.com/intl/es-419/earth/')
 #explicit wait
 explicitWait=WebDriverWait(driver,9)
-#the program waits until the button can be clicked and the does that
+#the program waits until the button can be clicked and then does that
 button = explicitWait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/header/div/nav[1]/ul[2]/li[2]/a')))
 button.click()
 time.sleep(10)
