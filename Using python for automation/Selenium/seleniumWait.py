02e25aaf96c9faa9a15e64237e068724ad8caa66
anudsa <anwardelgadosilva@gmail.com>
2023-08-31 22:48:02 -0600

    program that waits until element is clickable using selenium

---
+++ Using python for automation/Selenium/seleniumWait.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+driver = webdriver.Chrome()
+driver.get('https://www.google.com/intl/es-419/earth/')
+#explicit wait
+explicitWait=WebDriverWait(driver,9)
+#the program waits until the button can be clicked and the does that
+button = explicitWait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/header/div/nav[1]/ul[2]/li[2]/a')))
+button.click()
+time.sleep(10)
